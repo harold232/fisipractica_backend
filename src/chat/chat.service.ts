dc73fd8c8320827d1045efb4b207fe1a2b5d0ade
Gerson Aznarán <gersonec12@gmail.com>
2025-07-03 23:48:52 -0500

    feat(chat.gateway): mejorar manejo de errores y simplificar la obtención de user_id
    # Conflicts:
    #	package-lock.json
    #	package.json

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,26 +1,150 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Job } from 'src/job/entities/job.entity';
+import { Recruiter } from 'src/recruiter/entities/recruiter.entity';
+import { Student } from 'src/student/entities/student.entity';
+import { Repository } from 'typeorm';
 import { CreateChatDto } from './dto/create-chat.dto';
+import { FilterChatDto } from './dto/filter-chat.dto';
 import { UpdateChatDto } from './dto/update-chat.dto';
+import { Chat } from './entities/chat.entity';
 
 @Injectable()
 export class ChatService {
-  create(createChatDto: CreateChatDto) {
-    return 'This action adds a new chat';
+  constructor(
+    @InjectRepository(Chat)
+    private readonly chatRepository: Repository<Chat>,
+    @InjectRepository(Job)
+    private readonly jobRepository: Repository<Job>,
+    @InjectRepository(Student)
+    private readonly studentRepository: Repository<Student>,
+    @InjectRepository(Recruiter)
+    private readonly recruiterRepository: Repository<Recruiter>,
+  ) {}
+
+  async create(createChatDto: CreateChatDto): Promise<Chat> {
+    try {
+      const [chatExists, student, recruiter, job] = await Promise.all([
+        this.chatRepository.findOne({
+          where: {
+            student: { id: createChatDto.student_id },
+            recruiter: { id: createChatDto.recruiter_id },
+          },
+        }),
+        this.studentRepository.findOneBy({ id: createChatDto.student_id }),
+        this.recruiterRepository.findOneBy({ id: createChatDto.recruiter_id }),
+        this.jobRepository.findOneBy({ id: createChatDto.job_id }),
+      ]);
+
+      if (chatExists) {
+        return chatExists;
+      }
+      if (!student) {
+        throw new NotFoundException(
+          `Estudiante con id ${createChatDto.student_id} no encontrado`,
+        );
+      }
+      if (!recruiter) {
+        throw new NotFoundException(
+          `Reclutador con id ${createChatDto.recruiter_id} no encontrado`,
+        );
+      }
+      if (!job) {
+        throw new NotFoundException(
+          `Trabajo con id ${createChatDto.job_id} no encontrado`,
+        );
+      }
+
+      const chat = this.chatRepository.create({
+        ...createChatDto,
+        student,
+        recruiter,
+      });
+      return await this.chatRepository.save(chat);
+    } catch (error) {
+      console.error(error);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Error interno al crear chat.');
+    }
   }
 
-  findAll() {
-    return `This action returns all chat`;
+  async findAll(): Promise<Chat[]> {
+    try {
+      const chats = await this.chatRepository.find({
+        relations: ['student', 'recruiter'],
+      });
+      return chats;
+    } catch (error) {
+      console.error(error);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Error al listar los chats');
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} chat`;
+  async findOne(filter: FilterChatDto): Promise<Chat> {
+    try {
+      console.log(filter);
+      const chat = await this.chatRepository.findOne({
+        where: {
+          id: filter.id,
+          student: { id: filter.student_id },
+          recruiter: { id: filter.recruiter_id },
+          job_id: filter.job_id,
+        },
+        relations: ['student', 'recruiter'],
+      });
+      if (!chat) {
+        throw new NotFoundException(`Chat con id ${filter.id} no encontrado`);
+      }
+      return chat;
+    } catch (error) {
+      console.error(error.message);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Error al buscar el chat');
+    }
   }
 
-  update(id: number, updateChatDto: UpdateChatDto) {
-    return `This action updates a #${id} chat`;
+  async update(id: number, updateChatDto: UpdateChatDto): Promise<Chat> {
+    try {
+      const chat = await this.chatRepository.findOneBy({ id });
+      if (!chat) {
+        throw new NotFoundException(`Chat con id ${id} no encontrado`);
+      }
+      Object.assign(chat, updateChatDto);
+      return await this.chatRepository.save(chat);
+    } catch (error) {
+      console.error(error);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Error al actualizar el chat');
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} chat`;
+  async remove(id: number): Promise<Chat> {
+    try {
+      const chat = await this.chatRepository.findOneBy({ id });
+      if (!chat) {
+        throw new NotFoundException(`Chat con id ${id} no encontrado`);
+      }
+      return await this.chatRepository.remove(chat);
+    } catch (error) {
+      console.error(error);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Error al eliminar el chat');
+    }
   }
 }
