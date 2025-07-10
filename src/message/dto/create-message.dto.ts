5b80bfc6aefe36fdc5105752ce2075ce956aa93f
Gerson Aznarán <gerson.aznaran@unmsm.edu.pe>
2025-07-09 23:10:31 -0500

    feat(message): DTO para mensajes

--- src/message/dto/create-message.dto.ts
+++ src/message/dto/create-message.dto.ts
@@ -1,1 +1,12 @@
-export class CreateMessageDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateMessageDto {
+  @ApiProperty()
+  chat_id: number;
+
+  @ApiProperty()
+  message: string;
+
+  @ApiProperty({ required: false })
+  user_id?: number;
+}
