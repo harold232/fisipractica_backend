a2affe57636b7f27253f4d242a9ea9910cc5d222
Gerson Aznarán <gerson.aznaran@unmsm.edu.pe>
2025-07-09 23:10:07 -0500

    feat(message): Agregando entidad para mensaje

--- src/message/entities/message.entity.ts
+++ src/message/entities/message.entity.ts
@@ -1,1 +1,28 @@
-export class Message {}
+import { Chat } from 'src/chat/entities/chat.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Message {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Chat, (chat) => chat.messages)
+  @JoinColumn({ name: 'chat_id' })
+  chat: Chat;
+
+  @Column()
+  message: string;
+
+  @Column({ nullable: true })
+  user_id?: number;
+
+  @CreateDateColumn()
+  create_date: Date;
+}
