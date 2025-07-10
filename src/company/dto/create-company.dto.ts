63ba1c6eeecaf09c7c18e63150f3ef5fa83154b0
Harold Aquino <harold.aquino@unmsm.edu.pe>
2025-07-09 22:53:57 -0500

    Change: company dto

--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -1,1 +1,38 @@
-export class CreateCompanyDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateCompanyDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  address: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  website: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  location: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  color: string;
+
+  @ApiProperty({ type: 'string', format: 'binary', required: false })
+  @IsOptional()
+  photo: Express.Multer.File;
+}
