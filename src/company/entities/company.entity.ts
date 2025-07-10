29c4dc66229609ce89576f0f35c6eecfff20c8ec
Harold Aquino <harold.aquino@unmsm.edu.pe>
2025-07-09 22:53:19 -0500

    add: Entity company

--- src/company/entities/company.entity.ts
+++ src/company/entities/company.entity.ts
@@ -1,1 +1,49 @@
-export class Company {}
+import { Recruiter } from 'src/recruiter/entities/recruiter.entity';
+import { Job } from 'src/job/entities/job.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Company {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  website: string;
+
+  @Column()
+  location: string;
+
+  @Column({ nullable: true })
+  color: string;
+
+  @Column({ type: 'bytea', nullable: true })
+  photo: Uint8Array;
+
+  @CreateDateColumn()
+  create_date: Date;
+
+  @UpdateDateColumn()
+  update_date: Date;
+
+  @OneToMany(() => Recruiter, (recruiter) => recruiter.company)
+  recruiters: Recruiter[];
+
+  @OneToMany(() => Job, (job) => job.company)
+  jobs: Job[];
+}
